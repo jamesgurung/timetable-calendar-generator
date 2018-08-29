69f85f2be116aebabf1a18c83315333a591cffcd
James Gurung <jamesgurung@users.noreply.github.com>
2018-08-29 21:21:12 +0100

    Fixed iCal generation

--- src/CalendarWriters/IcalCalendarWriter.cs
+++ src/CalendarWriters/IcalCalendarWriter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,12 +23,16 @@ namespace makecal
       sb.AppendLine("BEGIN:VCALENDAR");
       sb.AppendLine("PRODID:-//github.com/jamesgurung/timetable-calendar-generator//makecal//EN");
       sb.AppendLine("VERSION:2.0");
+      sb.AppendLine("BEGIN:VTIMEZONE\nTZID:Europe/London\nBEGIN:DAYLIGHT\nTZOFFSETFROM:+0000\nTZOFFSETTO:+0100\nTZNAME:BST\nDTSTART:19700329T010000\n" +
+        "RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU\nEND:DAYLIGHT\nBEGIN:STANDARD\nTZOFFSETFROM:+0100\nTZOFFSETTO:+0000\nTZNAME:GMT\nDTSTART:19701025T020000\n" +
+        "RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU\nEND:STANDARD\nEND:VTIMEZONE");
       foreach (var calendarEvent in events)
       {
         sb.AppendLine("BEGIN:VEVENT");
+        sb.AppendLine("UID:" + Guid.NewGuid().ToString());
         sb.AppendLine("SUMMARY:" + calendarEvent.Title);
-        sb.AppendLine("DTSTART:" + calendarEvent.Start.ToString(dateFormat));
-        sb.AppendLine("DTEND:" + calendarEvent.End.ToString(dateFormat));
+        sb.AppendLine("DTSTART;TZID=Europe/London:" + calendarEvent.Start.ToString(dateFormat));
+        sb.AppendLine("DTEND;TZID=Europe/London:" + calendarEvent.End.ToString(dateFormat));
         if (!string.IsNullOrWhiteSpace(calendarEvent.Location))
         {
           sb.AppendLine("LOCATION:" + calendarEvent.Location);
